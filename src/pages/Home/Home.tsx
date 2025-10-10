eadff473134438d1a9a7bfa37eb615c6f2c59121
Thien An <thienantran.fit@gmail.com>
2025-10-10 18:04:58 +0700

    Support mobile UI

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -15,35 +15,34 @@ import { Footer } from '../../components/Footer'
 export const Home = () => {
   return (
     <div>
-    <div className="relative flex size-full min-h-screen flex-col bg-[#f8f9fc] group/design-root overflow-x-hidden">  
-      <div className="layout-container flex h-full grow flex-col">
-        <div style={{
-          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.8) 100%), url(${image10})`,
-          backgroundSize: 'cover',
-        }}>
-          <NavBar className='text-white bg-black bg-opacity-50' selectedTab={Tabs[0]} />
-          <HomeHeader />
-        </div>
-        
-
-        <div className="px-40 flex flex-1 justify-center py-5">
-          <div className="layout-content-container flex flex-col max-w-[980px] flex-1">
-            <OurStory />
-            <WhyLearnKendo />
-            
-            
-            <KendoClubsList />
-            <LatestNewsSection />
-            <Gallery />
-            <ExecutiveCommiteeMembers />
-            <HomeQA />
+      <div className="relative flex size-full min-h-screen flex-col bg-[#f8f9fc] group/design-root overflow-x-hidden">  
+        <div className="layout-container flex h-full grow flex-col">
+          {/* Hero Section with Background */}
+          <div 
+              className="relative bg-cover bg-center bg-no-repeat"
+              style={{
+                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.8) 100%), url(${image10})`,
+              }}
+            >
+            <NavBar className='text-white bg-black bg-opacity-50' selectedTab={Tabs[0]} />
+            <HomeHeader />
           </div>
+          
+          <div className="flex flex-1 justify-center py-5 px-4 sm:px-8 md:px-16 lg:px-24 xl:px-40">
+              <div className="layout-content-container flex flex-col w-full max-w-[980px] flex-1">
+                <OurStory />
+                <WhyLearnKendo />
+                <KendoClubsList />
+                <LatestNewsSection />
+                <Gallery />
+                <ExecutiveCommiteeMembers />
+                <HomeQA />
+              </div>
+            </div>
+
+          <Footer />
         </div>
-
-        <Footer />
       </div>
-    </div>
-
     </div>
   )
 }
